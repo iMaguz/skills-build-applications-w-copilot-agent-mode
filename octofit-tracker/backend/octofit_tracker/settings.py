d3e3ade7b4bbec82d045b53c355e084f4771014f
iMaguz <22669271+iMaguz@users.noreply.github.com>
2025-04-08 18:05:53 +0000

    .

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!f*pic+y76s@@!nt7)&)f2xyv%%e=7d!9^)_=#=+s_d8@^rh16
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[REPLACE-THIS-WITH-YOUR-CODESPACE-NAME]-8000.app.github.dev']
 
 
 # Application definition
